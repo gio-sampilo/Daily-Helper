895c0eb93c27e208e5c65d084915d62e45da9055
Gio Sampilo <sampilogd@gmail.com>
2022-06-07 16:00:07 -0700

    added save button to JS

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 $("#currentDay").text(moment().format('dddd, MMM Do YYYY'));
 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+
+    
