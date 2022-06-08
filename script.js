04e6f19f0c33bd5a6cb6b63e52832d27dfe969c4
Gio Sampilo <sampilogd@gmail.com>
2022-06-07 21:39:46 -0700

    Added set item and get item

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 $("#currentDay").text(moment().format('dddd, MMM Do YYYY'));
 
+
+$(document).ready(function () {
+
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
+
         localStorage.setItem(time, text);
     })
 
+
+
     
+    }
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
+    keepTrack();
+
+})
+
